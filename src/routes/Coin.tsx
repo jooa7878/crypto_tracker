fbcdb4f11519d3d22ff901aa9d6e2abec4a1efd1
sanghyeokahn <jooa7878@naver.com>
2022-03-01 15:41:37 +0900

    feat : fetching api

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,14 +1,52 @@
+import { useState } from "react";
 import { useParams } from "react-router";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
+
+const Loader = styled.span`
+  display: block;
+  text-align: center;
+`;
 
 interface Params {
   coinId: string;
 }
 
+interface RouteState {
+  name: string;
+}
+
 function Coin() {
   const { coinId } = useParams<Params>();
-  console.log(coinId);
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<RouteState>();
+  console.log(state.name);
 
-  return <h1>Coin</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading ..."}</Title>
+      </Header>
+      {loading ? <Loader>Loading ...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,110 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: #fff;
+  color: ${(props) => props.theme.bgColor};
+  padding: 20px;
+  margin-bottom: 10px;
+  border-radius: 15px;
+
+  a {
+    transition: color 0.2s ease-in;
+    display: flex;
+    padding: 20px;
+    align-items: center;
+  }
+
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
+
+const Loader = styled.span`
+  display: block;
+  text-align: center;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 function Coins() {
-  return <h1>Coins</h1>;
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await resp.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading ...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                  alt="coins_symbol"
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
 }
 
 export default Coins;
